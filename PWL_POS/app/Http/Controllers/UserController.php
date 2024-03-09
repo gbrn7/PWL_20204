f3695ef7855ede88df9c3af09798a40a09afea2d
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2024-03-09 13:53:54 +0700

    Praktikum-2.2_Pertemuan-4

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -31,9 +31,13 @@ class UserController extends Controller
         // $user = userModel::where('level_id',1)->first();
         // $user = userModel::firstWhere('level_id',1);
 
-        $user = userModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+        // $user = userModel::findOr(20, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+
+        // $user = userModel::findOrFail(1);
+
+        $user = userModel::where('username', 'manager9')->firstOrFail();
 
         return view('user', ['data' => $user]);
 
