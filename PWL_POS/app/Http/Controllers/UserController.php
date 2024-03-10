91201f8fa7b8d5ceb767d57ab6c041af7d1095a8
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2024-03-10 16:06:12 +0700

    Praktikum-2.6_Pertemuan 4

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -68,18 +68,118 @@ class UserController extends Controller
         // );
 
 
-        $user = userModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama'=> 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ]
-        );
+        // $user = userModel::firstOrNew(
+        //     [
+        //         'username' => 'manager33',
+        //         'nama'=> 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ]
+        // );
+
+        // $user->save();
+
+        // $user = userModel::firstOrNew(
+        //     [
+        //         'username' => 'manager55',
+        //         'nama'=> 'Manager55',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ]
+        // );
+
+        // $user->username = 'manager56';
+
+        // $user->isDirty();
+        // $user->isDirty('username');
+        // $user->isDirty('nama');
+        // $user->isDirty(['nama', 'username']);
+
+        // $user->isClean();
+        // $user->isClean('username');
+        // $user->isClean('nama');
+        // $user->isClean(['nama', 'username']);
+
+        // $user->save();
+
+        // $user->isDirty();
+        // $user->isClean();
+        // dd($user->isDirty());
+
+        // $user = userModel::create(
+        //     [
+        //         'username' => 'manager11',
+        //         'nama'=> 'Manager11',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ]
+        // );
+
+        // $user->username = 'manager12';
+
+        
+        
+        // $user->save();
+
+        // $user->wasChanged();
+        // $user->wasChanged('username');
+        // $user->wasChanged(['username', 'level_id']);
+        // $user->wasChanged('nama');
+        // $user->wasChanged(['nama', 'username']);
+        // dd( $user->wasChanged(['nama', 'username']));
+
+
+
+        $user = userModel::all();
+        return view('user', ['data' => $user]);
+
+    }
+
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id
+        ]);
+
+        return redirect('/user');
+    }
+
+    public function ubah($id)
+    {
+        $user = userModel::find($id);
+
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = userModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make($request->password);
+        $user->level_id = $request->level_id;
 
         $user->save();
 
-        return view('user', ['data' => $user]);
+        return redirect('/user');
+
+    }
+
+    public function hapus($id)
+    {
+        $user = userModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
 
     }
 }
