9b4ff6f466b21942d112e8b624e962381a47f43d
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2024-03-19 23:36:31 +0700

    Pertemuan 5

--- PWL_POS (Pertemuan-5)/app/Http/Controllers/KategoriController.php
+++ PWL_POS (Pertemuan-5)/app/Http/Controllers/KategoriController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\DataTables\KategoriDataTable;
+use App\Models\KategoriModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class KategoriController extends Controller
 {
-    public function index()
+    public function index(KategoriDataTable $dataTable)
     {
         // $data = [
         //     'kategori_kode' => 'SNK',
@@ -27,8 +29,25 @@ class KategoriController extends Controller
 
         // return 'Delete data berhasil. Jumlah data yand dihapus: '.$row.' baris';
 
-        $data = DB::table('m_kategori')->get();
+        // $data = DB::table('m_kategori')->get();
         
-        return view('kategori', ['data' => $data]);
+        // return view('kategori', ['data' => $data]);
+
+        return $dataTable->render('kategori.index');
+    }
+
+    public function create()
+    {
+        return view('kategori.create');
+    }
+
+    public function store(Request $request)
+    {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
+
+        return redirect('/kategori');
     }
 }
