eb0bedff1774edd2c0ff9b760f0ffcd6ea13998d
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2024-04-29 16:09:21 +0700

    pertemuan 10

---
+++ PWL_POS (Pertemuan-10)/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\Api\RegisterController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('/register', RegisterController::class)->name('register');
