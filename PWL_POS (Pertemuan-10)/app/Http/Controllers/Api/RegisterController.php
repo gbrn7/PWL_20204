afe769c8efea4e904c9c2b4bb84a33036ac56a5a
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2024-04-29 16:19:17 +0700

    fix(authenticate): fixed validator

--- PWL_POS (Pertemuan-10)/app/Http/Controllers/Api/RegisterController.php
+++ PWL_POS (Pertemuan-10)/app/Http/Controllers/Api/RegisterController.php
@@ -14,7 +14,7 @@ class RegisterController extends Controller
         $validator = Validator::make($request->all(), [
             'username' => 'required',
             'nama' => 'required',
-            'password' => 'required"min:5|confirmed',
+            'password' => 'required|min:5|confirmed',
             'level_id' => 'required'
         ]);
 
